33e27f4bc7fb71b654447b8b5dc0c4cbb1902988
David Mandelberg <david@mandelberg.org>
2022-06-13 20:44:45 -0400

    Start on plugin to remove lost already-leaving guests

---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenRCT2 Pathfinding Workarounds
+
+This [OpenRCT2](https://openrct2.org/) plugin works around some issues with the
+game's pathfinding algorithms. Currently it just looks for lost guests trying to
+leave the park, and removes them.
+
+This is not an officially supported Google product.
---
+++ pathfinding-workarounds.js
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function helpLostGuests() {
+  var guestsRemoved = 0;
+  map.getAllEntities('guest').forEach(function(guest) {
+    if (!guest.isInPark || !guest.isLost) {
+      return;
+    }
+
+    if (guest.getFlag('leavingPark')) {
+      guest.remove();
+      ++guestsRemoved;
+      return;
+    }
+
+    // TODO(dseomn): Figure out a way to help lost guests who aren't trying to
+    // leave the park. See
+    // https://github.com/OpenRCT2/OpenRCT2/discussions/17379 for issues with
+    // using guest.destination.
+  });
+  console.log('Removed ' + guestsRemoved + ' lost guests.');
+}
+
+function main() {
+  context.subscribe('interval.day', helpLostGuests);
+}
+
+registerPlugin({
+  name: 'Pathfinding Workarounds',
+  version: '0',
+  authors: ['David Mandelberg'],
+  type: 'remote',
+  licence: 'Apache-2.0',
+  minApiVersion: 52,
+  targetApiVersion: 52,
+  main: main,
+});
